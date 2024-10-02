e76516f37ed2928fc9558526bf04e3b22ef11a97
glucaci <gabriel.lucaci.dev@gmail.com>
2024-10-02 23:01:07 +0200

    Cleanup

--- src/Workflow/src/Bus.AzureQueueStorage/ServiceCollectionExtensions.cs
+++ src/Workflow/src/Bus.AzureQueueStorage/ServiceCollectionExtensions.cs
@@ -12,22 +12,4 @@ public static class ServiceCollectionExtensions
         options.UseDistributedLockManager(sp => new AzureLockManager(connectionString, sp.GetService<ILoggerFactory>()));
         return options;
     }
-
-    public static WorkflowOptions UseAzureStorageQueue(this WorkflowOptions options, string connectionString)
-    {
-        options.UseQueueProvider(sp => new AzureStorageQueueProvider(connectionString, sp.GetService<ILoggerFactory>()));
-        return options;
-    }
-
-    public static WorkflowOptions UseAzureServiceBus(this WorkflowOptions options, string connectionString)
-    {
-        options.UseQueueProvider(sp => new AzureStorageQueueProvider(connectionString, sp.GetService<ILoggerFactory>()));
-        return options;
-    }
-
-    public static WorkflowOptions UseAzureStorageDistributedLock(this WorkflowOptions options, string connectionString)
-    {
-        options.UseDistributedLockManager(sp => new AzureLockManager(connectionString, sp.GetService<ILoggerFactory>()));
-        return options;
-    }
 }
