0d52339397eb06eb4508afcef72eb98048d15032
glucaci <gabriel.lucaci.dev@gmail.com>
2024-10-03 17:29:20 +0200

    remove queueing through IQueueProvider

--- src/Workflow/src/Abstractions/IQueueProvider.cs
+++ src/Workflow/src/Abstractions/IQueueProvider.cs
@@ -2,8 +2,6 @@
 
 public interface IQueueProvider : IDisposable
 {
-    Task QueueWork(string id, QueueType queue);
-
     Task<string> DequeueWork(QueueType queue, CancellationToken cancellationToken);
 
     bool IsDequeueBlocking { get; }
